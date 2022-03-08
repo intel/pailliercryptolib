d5fa36ef632e4c14aa171c228c6360847c60e56c
Souza, Fillipe <fillipe.souza@intel.com>
2022-03-07 17:21:19 -0800

    Add modular exponentiation interface to support generic types.

--- he_qat_bn_ops.c
+++ he_qat_bn_ops.c
@@ -36,7 +36,7 @@ static void lnModExpCallback(void* pCallbackTag,  // This type can be variable
             if (pOpData == request->op_data) {
                 //	      printf("pOpData is same as input\n");
                 // Mark request as complete or ready to be used
-                request->request_status = HE_QAT_READY;
+                request->request_status = HE_QAT_STATUS_READY;
 
                 BIGNUM* r = BN_bin2bn(request->op_result.pData,
                                       request->op_result.dataLenInBytes,
@@ -167,7 +167,7 @@ void* start_perform_op(void* _inst_config) {
 
     CpaStatus status = CPA_STATUS_FAIL;
 
-    // If called again, wait
+    // Start from zero or restart after stop_perform_op
     pthread_mutex_lock(&config->mutex);
     while (config->active) pthread_cond_wait(&config->ready, &config->mutex);
 
@@ -219,17 +219,15 @@ void* start_perform_op(void* _inst_config) {
             // Realize the type of operation from data
             switch (request->op_type) {
             // Select appropriate action
-            case HE_QAT_MODEXP:
-                //    printf("Perform HE_QAT_MODEXP\n");
+            case HE_QAT_OP_MODEXP:
                 status = cpaCyLnModExp(config->inst_handle, lnModExpCallback,
                                        (void*)request,
                                        (CpaCyLnModExpOpData*)request->op_data,
                                        &request->op_result);
                 retry++;
                 break;
-            case HE_QAT_NO_OP:
+            case HE_QAT_OP_NONE:
             default:
-                //		    printf("HE_QAT_NO_OP\n");
                 retry = HE_QAT_MAX_RETRY;
                 break;
             }
@@ -424,7 +422,7 @@ HE_QAT_STATUS bnModExpPerformOp(BIGNUM* r, BIGNUM* b, BIGNUM* e, BIGNUM* m,
         return HE_QAT_STATUS_FAIL;
     }
 
-    request->op_type = HE_QAT_MODEXP;
+    request->op_type = HE_QAT_OP_MODEXP;
     request->op_status = status;
     request->op_output = (void*)r;
 
@@ -454,7 +452,7 @@ void getBnModExpRequest(unsigned int batch_size) {
         // to complete processing
         pthread_mutex_lock(
             &task->mutex);  // mutex only needed for the conditional variable
-        while (HE_QAT_READY != task->request_status)
+        while (HE_QAT_STATUS_READY != task->request_status)
             pthread_cond_wait(&task->ready, &task->mutex);
         // Free up QAT temporary memory
         CpaCyLnModExpOpData* op_data = (CpaCyLnModExpOpData*)task->op_data;
@@ -511,3 +509,133 @@ task->op_data; if (op_data) { PHYS_CONTIG_FREE(op_data->base.pData);
     return ;
 }
 */
+
+/// @brief
+/// @function
+/// Callback function for lnIppModExpPerformOp. It performs any data processing
+/// required after the modular exponentiation.
+static void HE_QAT_bnModExpCallback(void* pCallbackTag,  // This type can be variable
+                             CpaStatus status,
+                             void* pOpData,  // This is fixed -- please swap it
+                             CpaFlatBuffer* pOut) {
+    HE_QAT_TaskRequest* request = NULL;
+
+    // Check if input data for the op is available and do something
+    if (NULL != pCallbackTag) {
+        // Read request data
+        request = (HE_QAT_TaskRequest*)pCallbackTag;
+
+        // Collect the device output in pOut
+        request->op_status = status;
+        if (CPA_STATUS_SUCCESS == status) {
+            if (pOpData == request->op_data) {
+                //	      printf("pOpData is same as input\n");
+                // Mark request as complete or ready to be used
+                request->request_status = HE_QAT_STATUS_READY;
+		// Copy compute results to output destination
+                memcpy(request->op_output, request->op_result.pData, request->op_result.dataLenInBytes);                
+            } else {
+                //	      printf("pOpData is NOT same as input\n");
+                request->request_status = HE_QAT_STATUS_FAIL;
+            }
+        }
+        // Make it synchronous and blocking
+        pthread_cond_signal(&request->ready);
+        COMPLETE((struct COMPLETION_STRUCT*)&request->callback);
+    }
+
+    return;
+}
+
+HE_QAT_STATUS HE_QAT_bnModExp(unsigned char* r, unsigned char* b, unsigned char* e, unsigned char* m, int nbits) {
+    // Unpack data and copy to QAT friendly memory space
+    int len = nbits / 8;
+
+    if (NULL == r) return HE_QAT_STATUS_INVALID_PARAM;
+    if (NULL == b) return HE_QAT_STATUS_INVALID_PARAM;
+    if (NULL == e) return HE_QAT_STATUS_INVALID_PARAM;
+    if (NULL == m) return HE_QAT_STATUS_INVALID_PARAM;
+
+    Cpa8U* pBase = NULL;
+    Cpa8U* pModulus = NULL;
+    Cpa8U* pExponent = NULL;
+
+
+    // TODO(fdiasmor): Try it with 8-byte alignment.
+    CpaStatus status = CPA_STATUS_FAIL;
+    status = PHYS_CONTIG_ALLOC(&pBase, len);
+    if (CPA_STATUS_SUCCESS == status && NULL != pBase) {
+        memcpy(pBase, b, len);
+    } else {
+        printf("Contiguous memory allocation failed for pBase.\n");
+        return HE_QAT_STATUS_FAIL;
+    }
+
+    status = PHYS_CONTIG_ALLOC(&pExponent, len);
+    if (CPA_STATUS_SUCCESS == status && NULL != pExponent) {
+        memcpy(pExponent, e, len);
+    } else {
+        printf("Contiguous memory allocation failed for pBase.\n");
+        return HE_QAT_STATUS_FAIL;
+    }
+
+    status = PHYS_CONTIG_ALLOC(&pModulus, len);
+    if (CPA_STATUS_SUCCESS == status && NULL != pModulus) {
+        memcpy(pModulus, m, len);
+    } else {
+        printf("Contiguous memory allocation failed for pBase.\n");
+        return HE_QAT_STATUS_FAIL;
+    }
+
+    // HE_QAT_TaskRequest request = 
+    //           HE_QAT_PACK_MODEXP_REQUEST(pBase, pExponent, pModulus, r)
+    // Pack it as a QAT Task Request
+    HE_QAT_TaskRequest* request =
+        (HE_QAT_TaskRequest*)calloc(1, sizeof(HE_QAT_TaskRequest));
+    if (NULL == request) {
+        printf(
+            "HE_QAT_TaskRequest memory allocation failed in "
+            "bnModExpPerformOp.\n");
+        return HE_QAT_STATUS_FAIL;
+    }
+    
+    CpaCyLnModExpOpData* op_data =
+        (CpaCyLnModExpOpData*)calloc(1, sizeof(CpaCyLnModExpOpData));
+    if (NULL == op_data) {
+        printf("Cpa memory allocation failed in bnModExpPerformOp.\n");
+        return HE_QAT_STATUS_FAIL;
+    }
+    op_data->base.pData = pBase;
+    op_data->base.dataLenInBytes = len;
+    op_data->exponent.pData = pExponent;
+    op_data->exponent.dataLenInBytes = len;
+    op_data->modulus.pData = pModulus;
+    op_data->modulus.dataLenInBytes = len;
+    request->op_data = (void*)op_data;
+
+    status = PHYS_CONTIG_ALLOC(&request->op_result.pData, len);
+    if (CPA_STATUS_SUCCESS == status && NULL != request->op_result.pData) {
+        request->op_result.dataLenInBytes = len;
+    } else {
+        printf(
+            "CpaFlatBuffer.pData memory allocation failed in "
+            "bnModExpPerformOp.\n");
+        return HE_QAT_STATUS_FAIL;
+    }
+
+    request->op_type = HE_QAT_OP_MODEXP;
+    request->op_status = status;
+    request->op_output = (void*)r;
+
+    // Ensure calls are synchronous and blocking
+    pthread_mutex_init(&request->mutex, NULL);
+    pthread_cond_init(&request->ready, NULL);
+
+    // Submit request using producer function
+    // printf("Submit request \n");
+    submit_request(&he_qat_buffer, (void*)request);
+    // printf("Submitted\n");
+
+    return HE_QAT_STATUS_SUCCESS;
+}
+
