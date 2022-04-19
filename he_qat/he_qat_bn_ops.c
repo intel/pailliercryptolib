82809478f2163107fd2dae40e8a6812d7e2e40e1
Souza, Fillipe <fillipe.souza@intel.com>
2022-04-18 18:10:46 -0700

    Complete multi-threading support with distributed outstanding buffers.
    + Add acquire buffer interface.
    + Add release buffer interface.
    + Add bnModExp multi-threaded interface.
    + Add function to complete the request pulling mechanism from outstanding buffers.
    + Use the new outstanding buffer type.

--- he_qat/he_qat_bn_ops.c
+++ he_qat/he_qat_bn_ops.c
@@ -27,6 +27,7 @@ double time_taken = 0.0;
 // Global buffer for the runtime environment
 HE_QAT_RequestBuffer he_qat_buffer;
 HE_QAT_RequestBufferList outstanding_buffer;
+HE_QAT_OutstandingBuffer outstanding;
 
 /// @brief
 /// @function
@@ -112,6 +113,7 @@ static void submit_request_list(HE_QAT_RequestBuffer* _buffer,
     printf("Wait lock write request. [buffer size: %d]\n", _buffer->count);
 #endif
 
+    // Wait until buffer can accomodate the number of input requests
     while (_buffer->count >= HE_QAT_BUFFER_SIZE ||
            (HE_QAT_BUFFER_SIZE - _buffer->count + 1) < _requests->count)
         pthread_cond_wait(&_buffer->any_free_slot, &_buffer->mutex);
@@ -132,26 +134,12 @@ static void submit_request_list(HE_QAT_RequestBuffer* _buffer,
 #endif
 }
 
-static void* get_buffer(HE_QAT_RequestBufferList* _list, unsigned int index) {
-    if (0 == index) return _list->data;
-    unsigned int j = 0;
-    void* curr = _list->next;
-    while (curr) {
-        if (index == ++j) break;
-        curr = _list->next;
-    }
-    return curr;
-}
-
-static void* add_buffer(HE_QAT_RequestBufferList* _list) { return NULL; }
-
-static void del_buffer() { return; }
-
 /// @brief
 /// @function
 /// Thread-safe producer implementation for the shared outstanding request
 /// buffer that stores request from multiple threads.
 /// Stores requests in a buffer that will be sent to the HE QAT buffer.
+/// @unused
 static void push_request(HE_QAT_RequestBufferList* _outstanding_buffer,
                          void* args, unsigned int num_requests = 1) {
 #ifdef HE_QAT_DEBUG
@@ -208,6 +196,7 @@ static void push_request(HE_QAT_RequestBufferList* _outstanding_buffer,
 /// @function
 /// Thread-safe consumer implementation for the shared request buffer.
 /// Read requests from a buffer to finally offload the work to QAT devices.
+/// Supported in single-threaded or multi-threaded mode.
 static HE_QAT_TaskRequest* read_request(HE_QAT_RequestBuffer* _buffer) {
     void* item = NULL;
 
@@ -219,9 +208,6 @@ static HE_QAT_TaskRequest* read_request(HE_QAT_RequestBuffer* _buffer) {
     assert(_buffer->count > 0);
 
     item = _buffer->data[_buffer->next_data_slot++];
-
-    // TODO(fdiasmor): for multithreading mode
-    // Make copy of request so that the buffer can be reused
 
     _buffer->next_data_slot %= HE_QAT_BUFFER_SIZE;
     _buffer->count--;
@@ -236,6 +222,7 @@ static HE_QAT_TaskRequest* read_request(HE_QAT_RequestBuffer* _buffer) {
 /// @function
 /// Thread-safe consumer implementation for the shared request buffer.
 /// Read requests from a buffer to finally offload the work to QAT devices.
+/// @future: Meant for multi-threaded mode.
 static void read_request_list(HE_QAT_TaskRequestList* _requests,
                               HE_QAT_RequestBuffer* _buffer) {
     if (NULL == _requests) return;
@@ -265,8 +252,10 @@ static void read_request_list(HE_QAT_TaskRequestList* _requests,
 /// @function
 /// Thread-safe consumer implementation for the shared request buffer.
 /// Read requests from a buffer to finally offload the work to QAT devices.
+/// @deprecated
 static void pull_request(HE_QAT_TaskRequestList* _requests,
-                         HE_QAT_RequestBufferList* _outstanding_buffer,
+                         //HE_QAT_OutstandingBuffer *_outstanding_buffer,
+		         HE_QAT_RequestBufferList* _outstanding_buffer,
                          unsigned int max_num_requests = 1) {
     if (NULL == _requests) return;
 
@@ -309,6 +298,115 @@ static void pull_request(HE_QAT_TaskRequestList* _requests,
     return;
 }
 
+static void pull_outstanding_requests(HE_QAT_TaskRequestList* _requests,
+                         HE_QAT_OutstandingBuffer *_outstanding_buffer,
+                         unsigned int max_num_requests = 1) {
+
+    if (NULL == _requests) return;
+    _requests->count = 0;
+
+    // for now, only one thread can change next_ready_buffer
+    // so no need for sync tools
+
+    // Select an outstanding buffer to pull requests and add them into the processing queue (internal buffer)
+    pthread_mutex_lock(&_outstanding_buffer->mutex);
+    // Wait until next outstanding buffer becomes available for use
+    while (outstanding.busy_count <= 0)
+        pthread_cond_wait(&_outstanding_buffer->any_ready_buffer, &_outstanding_buffer->mutex);
+    
+    int any_ready = 0;
+    unsigned int index =_outstanding_buffer->next_ready_buffer; // no fairness
+    for (unsigned int i = 0; i < HE_QAT_BUFFER_COUNT; i++) {
+	index = i; // ensure fairness
+	if (_outstanding_buffer->ready_buffer[index] 
+			&& _outstanding_buffer->buffer[index].count) { //sync with mutex at interface
+            any_ready = 1;
+	    break;
+	}
+	//index = (index + 1) % HE_QAT_BUFFER_COUNT; 
+    }
+    // Ensures it gets picked once only
+    pthread_mutex_unlock(&_outstanding_buffer->mutex);
+
+    if (!any_ready) return;
+
+    // Extract outstanding requests from outstanding buffer 
+    // (this is the only function that reads from outstanding buffer, from a single thread)
+    pthread_mutex_lock(&_outstanding_buffer->buffer[index].mutex);
+    // This conditional waiting may not be required
+    // Wait while buffer is empty
+    while (_outstanding_buffer->buffer[index].count <= 0) {
+        pthread_cond_wait(&_outstanding_buffer->buffer[index].any_more_data,
+                          &_outstanding_buffer->buffer[index].mutex);
+    }
+    assert(_outstanding_buffer->buffer[index].count > 0);
+    // 
+
+    unsigned int num_requests = (_outstanding_buffer->buffer[index].count < max_num_requests)
+                                    ? _outstanding_buffer->buffer[index].count : max_num_requests;
+
+    assert(num_requests <= HE_QAT_BUFFER_SIZE);
+
+    for (unsigned int i = 0; i < num_requests; i++) {
+        _requests->request[i] = _outstanding_buffer->buffer[index]
+		.data[_outstanding_buffer->buffer[index].next_data_slot];
+        _outstanding_buffer->buffer[index].count--;
+        _outstanding_buffer->buffer[index].next_data_slot++;
+        _outstanding_buffer->buffer[index].next_data_slot %= HE_QAT_BUFFER_SIZE;
+    }
+    _requests->count = num_requests;
+
+    pthread_cond_signal(&_outstanding_buffer->buffer[index].any_free_slot);
+    pthread_mutex_unlock(&_outstanding_buffer->buffer[index].mutex);
+
+    // ---------------------------------------------------------------------------
+    // Notify there is an outstanding buffer in ready for the processing queue
+//    pthread_mutex_lock(&_outstanding_buffer->mutex);
+//
+//    _outstanding_buffer->ready_count--;
+//    _outstanding_buffer->ready_buffer[index] = 0;
+//
+//    pthread_cond_signal(&_outstanding_buffer->any_free_buffer);
+//    pthread_mutex_unlock(&_outstanding_buffer->mutex);
+    
+    return;
+}
+
+// Frontend for multithreading support
+HE_QAT_STATUS acquire_bnModExp_buffer(unsigned int *_buffer_id) { 
+    if (NULL == _buffer_id) return HE_QAT_STATUS_INVALID_PARAM;
+
+    pthread_mutex_lock(&outstanding.mutex);
+    // Wait until next outstanding buffer becomes available for use
+    while (outstanding.busy_count >= HE_QAT_BUFFER_COUNT)
+        pthread_cond_wait(&outstanding.any_free_buffer, &outstanding.mutex);
+
+    assert(outstanding.busy_count < HE_QAT_BUFFER_COUNT);
+ 
+    // find next available
+    unsigned int next_free_buffer = outstanding.next_free_buffer;
+    for (unsigned int i = 0; i < HE_QAT_BUFFER_COUNT; i++) {
+        if (outstanding.free_buffer[next_free_buffer]) {
+           outstanding.free_buffer[next_free_buffer] = 0;
+	   *_buffer_id = next_free_buffer;
+	   break;
+	}
+	next_free_buffer = (next_free_buffer + 1) % HE_QAT_BUFFER_COUNT;
+    }
+
+    outstanding.next_free_buffer = (*_buffer_id + 1) % HE_QAT_BUFFER_COUNT;
+    outstanding.next_ready_buffer = *_buffer_id;
+    outstanding.ready_buffer[*_buffer_id] = 1;
+    outstanding.busy_count++;
+    // busy meaning: 
+    // taken by a thread, enqueued requests, in processing, waiting results
+
+    pthread_cond_signal(&outstanding.any_ready_buffer);
+    pthread_mutex_unlock(&outstanding.mutex);
+
+    return HE_QAT_STATUS_SUCCESS;
+}
+
 static void* schedule_requests(void* state) {
     if (NULL == state) {
         printf("Failed at buffer_manager: argument is NULL.\n");
@@ -324,12 +422,12 @@ static void* schedule_requests(void* state) {
     outstading_requests->count = 0;
 
     // this thread should receive signal from context to exit
-    while (active) {
+    while (*active) {
         // collect a set of requests from the outstanding buffer
-        pull_request(&outstanding_requests, &outstanding_buffer,
-                     HE_QAT_BUFFER_SIZE);
+        //pull_request(&outstanding_requests, &outstanding_buffer, HE_QAT_BUFFER_SIZE);
+        pull_outstanding_requests(&outstanding_requests, &outstanding, HE_QAT_BUFFER_SIZE);
         // submit them to the HE QAT buffer for offloading
-        submit_request(&he_qat_buffer, &outstanding_requests);
+        submit_request_list(&he_qat_buffer, &outstanding_requests);
     }
 
     pthread_exit(NULL);
@@ -660,6 +758,81 @@ HE_QAT_STATUS bnModExpPerformOp(BIGNUM* r, BIGNUM* b, BIGNUM* e, BIGNUM* m,
     return HE_QAT_STATUS_SUCCESS;
 }
 
+// Assume allocate_bnModExp_buffer(&_buffer_id) to be called first 
+// to secure and allocate an outstanding buffer for the target thread.
+// Multithread support for release_bnModExp_buffer(_buffer_id, batch_size)
+void release_bnModExp_buffer(int _buffer_id, unsigned int _batch_size) {
+    unsigned int j = 0;
+    
+#ifdef HE_QAT_PERF
+    gettimeofday(&start_time, NULL);
+#endif
+    do {
+        // Buffer read may be safe for single-threaded blocking calls only.
+        // Note: Not tested on multithreaded environment.
+        HE_QAT_TaskRequest* task =
+            (HE_QAT_TaskRequest*) outstanding.buffer[_buffer_id].data[j];
+
+        //if (NULL == task)
+        //   continue;
+
+        // Block and synchronize: Wait for the most recently offloaded request
+        // to complete processing
+        pthread_mutex_lock(
+            &task->mutex);  // mutex only needed for the conditional variable
+        while (HE_QAT_STATUS_READY != task->request_status)
+            pthread_cond_wait(&task->ready, &task->mutex);
+
+#ifdef HE_QAT_PERF
+        time_taken = (task->end.tv_sec - task->start.tv_sec) * 1e6;
+        time_taken =
+            (time_taken + (task->end.tv_usec - task->start.tv_usec));  //*1e-6;
+        printf("%u time: %.1lfus\n", j, time_taken);
+#endif
+
+        // Free up QAT temporary memory
+        CpaCyLnModExpOpData* op_data = (CpaCyLnModExpOpData*)task->op_data;
+        if (op_data) {
+            PHYS_CONTIG_FREE(op_data->base.pData);
+            PHYS_CONTIG_FREE(op_data->exponent.pData);
+            PHYS_CONTIG_FREE(op_data->modulus.pData);
+        }
+        free(task->op_data);
+        task->op_data = NULL;
+        if (task->op_result.pData) {
+            PHYS_CONTIG_FREE(task->op_result.pData);
+        }
+
+        // Move forward to wait for the next request that will be offloaded
+        pthread_mutex_unlock(&task->mutex);
+
+        // Fix segmentation fault?
+        free(outstanding.buffer[_buffer_id].data[j]);
+        outstanding.buffer[_buffer_id].data[j] = NULL; 
+    } while (++j < batch_size);
+
+#ifdef HE_QAT_PERF
+    gettimeofday(&end_time, NULL);
+    time_taken = (end_time.tv_sec - start_time.tv_sec) * 1e6;
+    time_taken =
+        (time_taken + (end_time.tv_usec - start_time.tv_usec));  //*1e-6;
+    printf("Batch Wall Time: %.1lfus\n", time_taken);
+#endif
+
+    // Release outstanding buffer for usage by another thread
+    pthread_mutex_lock(&outstanding.mutex);
+
+    outstanding.next_free_buffer = _buffer_id;
+    outstanding.ready_buffer[_buffer_id] = 0;
+    outstanding.free_buffer[_buffer_id] = 1;
+    outstanding.busy_count--;
+
+    pthread_cond_signal(&outstanding.any_free_buffer);
+    pthread_mutex_unlock(&outstanding.mutex);
+
+    return;
+}
+
 // Maybe it will be useful to pass the number of requests to retrieve
 // Pass post-processing function as argument to bring output to expected type
 void getBnModExpRequest(unsigned int batch_size) {
@@ -869,6 +1042,109 @@ HE_QAT_STATUS HE_QAT_bnModExp(unsigned char* r, unsigned char* b,
 
     // Submit request using producer function
     submit_request(&he_qat_buffer, (void*)request);
+
+    return HE_QAT_STATUS_SUCCESS;
+}
+
+HE_QAT_STATUS HE_QAT_bnModExp_MT(int _buffer_id, unsigned char* r, unsigned char* b,
+                              unsigned char* e, unsigned char* m, int nbits) {
+#ifdef HE_QAT_DEBUG
+    static unsigned long long req_count = 0;
+    // printf("Wait lock write request. [buffer size: %d]\n",_buffer->count);
+#endif
+    // Unpack data and copy to QAT friendly memory space
+    int len = nbits / 8;
+
+    if (NULL == r) return HE_QAT_STATUS_INVALID_PARAM;
+    if (NULL == b) return HE_QAT_STATUS_INVALID_PARAM;
+    if (NULL == e) return HE_QAT_STATUS_INVALID_PARAM;
+    if (NULL == m) return HE_QAT_STATUS_INVALID_PARAM;
+
+    Cpa8U* pBase = NULL;
+    Cpa8U* pModulus = NULL;
+    Cpa8U* pExponent = NULL;
+
+    // TODO(fdiasmor): Try it with 8-byte alignment.
+    CpaStatus status = CPA_STATUS_FAIL;
+    // status = PHYS_CONTIG_ALLOC(&pBase, len);
+    status = PHYS_CONTIG_ALLOC_ALIGNED(&pBase, len, 8);
+    if (CPA_STATUS_SUCCESS == status && NULL != pBase) {
+        memcpy(pBase, b, len);
+    } else {
+        printf("Contiguous memory allocation failed for pBase.\n");
+        return HE_QAT_STATUS_FAIL;
+    }
+
+    // status = PHYS_CONTIG_ALLOC(&pExponent, len);
+    status = PHYS_CONTIG_ALLOC_ALIGNED(&pExponent, len, 8);
+    if (CPA_STATUS_SUCCESS == status && NULL != pExponent) {
+        memcpy(pExponent, e, len);
+    } else {
+        printf("Contiguous memory allocation failed for pBase.\n");
+        return HE_QAT_STATUS_FAIL;
+    }
+
+    // status = PHYS_CONTIG_ALLOC(&pModulus, len);
+    status = PHYS_CONTIG_ALLOC_ALIGNED(&pModulus, len, 8);
+    if (CPA_STATUS_SUCCESS == status && NULL != pModulus) {
+        memcpy(pModulus, m, len);
+    } else {
+        printf("Contiguous memory allocation failed for pBase.\n");
+        return HE_QAT_STATUS_FAIL;
+    }
+
+    // HE_QAT_TaskRequest request =
+    //           HE_QAT_PACK_MODEXP_REQUEST(pBase, pExponent, pModulus, r)
+    // Pack it as a QAT Task Request
+    HE_QAT_TaskRequest* request =
+        (HE_QAT_TaskRequest*)calloc(1, sizeof(HE_QAT_TaskRequest));
+    if (NULL == request) {
+        printf(
+            "HE_QAT_TaskRequest memory allocation failed in "
+            "bnModExpPerformOp.\n");
+        return HE_QAT_STATUS_FAIL;
+    }
+
+    CpaCyLnModExpOpData* op_data =
+        (CpaCyLnModExpOpData*)calloc(1, sizeof(CpaCyLnModExpOpData));
+    if (NULL == op_data) {
+        printf("Cpa memory allocation failed in bnModExpPerformOp.\n");
+        return HE_QAT_STATUS_FAIL;
+    }
+    op_data->base.pData = pBase;
+    op_data->base.dataLenInBytes = len;
+    op_data->exponent.pData = pExponent;
+    op_data->exponent.dataLenInBytes = len;
+    op_data->modulus.pData = pModulus;
+    op_data->modulus.dataLenInBytes = len;
+    request->op_data = (void*)op_data;
+
+    // status = PHYS_CONTIG_ALLOC(&request->op_result.pData, len);
+    status = PHYS_CONTIG_ALLOC_ALIGNED(&request->op_result.pData, len, 8);
+    if (CPA_STATUS_SUCCESS == status && NULL != request->op_result.pData) {
+        request->op_result.dataLenInBytes = len;
+    } else {
+        printf(
+            "CpaFlatBuffer.pData memory allocation failed in "
+            "bnModExpPerformOp.\n");
+        return HE_QAT_STATUS_FAIL;
+    }
+
+    request->op_type = HE_QAT_OP_MODEXP;
+    request->callback_func = (void*)HE_QAT_bnModExpCallback;
+    request->op_status = status;
+    request->op_output = (void*)r;
+
+    // Ensure calls are synchronized at exit (blocking)
+    pthread_mutex_init(&request->mutex, NULL);
+    pthread_cond_init(&request->ready, NULL);
+
+#ifdef HE_QAT_DEBUG
+    printf("BN ModExp interface call for request #%llu\n", ++req_count);
+#endif
+
+    // Submit request using producer function
+    submit_request(&outstanding.buffer[_buffer_id], (void*)request);
 
     return HE_QAT_STATUS_SUCCESS;
 }
