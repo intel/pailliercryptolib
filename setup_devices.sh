bbcb920453372e973bd0fed8864276e9d3d465f0
Souza, Fillipe <fillipe.souza@intel.com>
2022-09-20 18:31:00 -0700

    Update strict to setup devices.
    Signed-off-by: Souza, Fillipe <fillipe.souza@intel.com>

--- setup_devices.sh
+++ setup_devices.sh
@@ -1,35 +1,54 @@
-#!/bin/env bash
+#!/bin/bash
 
-num_phys_dev=8   # num_dev=$(lspci -d 8086:4940 | wc -l)
-conf_virt_func=0 # total_virt_func=$(lspci -d 8086:4941 | wc -l)
-num_virt_func=0  # conf_virt_func=`expr $total_virt_func / $num_dev`
+num_phys_dev=$(lspci -d 8086:4940 | wc -l) 
+if [ $num_phys_dev -eq 0 ]; then
+  echo "No QAT Device Found !"
+  exit
+else
+  echo "$num_phys_dev QAT Devices Found !"	
+fi
+
+total_virt_func=$(lspci -d 8086:4941 | wc -l) 
+num_virt_func=`expr $total_virt_func / $num_phys_dev`
 dev_step=1
 
 if [ $# -eq 0 ]; then
-  echo "Parameters:"
-  echo "-- num_phys_dev: Number of physical devices to be active. (default: 8)"
-  echo "-- conf_virt_func: Number of configured virtual functions per device. (default: 0)"
-  echo "-- num_virt_func: Number of virtual functions to be active per device. (default: 0)"
-  echo "./setup_devices <num_phys_dev> <num_virt_inst> <conf_virt_inst>"
+  echo "Usage: ./setup_devices <num_phys_dev> <num_virt_inst> <conf_virt_inst>"
+  echo "   Parameters:"
+  echo "   -- num_phys_dev:   Number of physical devices to be active. (default: auto)"
+  echo "   -- conf_virt_func: Number of configured virtual functions per device. (default: 0)"
+  echo "   -- num_virt_func:  Number of virtual functions to be active per device. (default: 0)"
 fi
 
+nphysdev=$num_phys_dev
 if [ -n "$1" ]; then
-  num_phys_dev=$1
+  nphysdev=$1
+  if [ $nphysdev -gt $num_phys_dev ]; then
+    nphysdev=$num_phys_dev
+  fi
 fi
+
+conf_virt_func=0
+# Check if virtual function is enabled
+if [ $num_virt_func -gt 0 ]; then
+  conf_virt_func=1
+fi 
 
 if [ -n "$2" ]; then
   conf_virt_func=$2
+  # if user attempts to request higher than available
+  if [ $conf_virt_func -gt $num_virt_func ]; then
+    conf_virt_func=$num_virt_func
+  fi
 fi
 
-if [ -n "$3" ]; then
-  num_virt_func=$3
+start=0
+# If on virtualization mode
+if [ $num_virt_func -gt 0 & $conf_virt_func -gt 0 ]; then 
+  start=$num_phys_dev
+  dev_step=$num_virt_func
 fi
 
-if [ $conf_virt_func -gt 0 ]; then 
-  dev_step=$conf_virt_func
-fi
-
-start=$num_phys_dev
 stop=`expr $num_phy_dev \\* $conf_virt_func`
 stop=`expr $start + $stop`
 step=$dev_step
@@ -50,7 +69,7 @@ do
   sudo adf_ctl qat_dev$start up;
   # start up additional instances mapped to the same physical device
   j=1;
-  while [ $j -lt $num_virt_func ]; 
+  while [ $j -lt $conf_virt_func ]; 
   do
     dev_id=`expr $j + $start`;
     sudo adf_ctl qat_dev$dev_id up;
