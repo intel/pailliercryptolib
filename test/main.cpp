8f2754056beec2a171f2a273aae65d7b39c5cd33
Souza, Fillipe <fillipe.souza@intel.com>
2022-10-12 18:28:04 -0700

    Test QAT context status interface.
    Signed-off-by: Souza, Fillipe <fillipe.souza@intel.com>

--- test/main.cpp
+++ test/main.cpp
@@ -9,6 +9,16 @@
 int main(int argc, char** argv) {
 #ifdef IPCL_USE_QAT
   ipcl::initializeContext("QAT");
+  
+  if (ipcl::isQATActive())
+     std::cout << "QAT Context: ACTIVE" << std::endl;
+  else 
+    std::cout << "Error: QAT Context INACTIVE." << std::endl;
+
+  if (ipcl::isQATRunning())
+     std::cout << "QAT Instances: RUNNING" << std::endl;
+  else 
+    std::cout << "Error: QAT Instances NOT RUNNING." << std::endl;
 #else
   ipcl::initializeContext("default");
 #endif  // IPCL_USE_QAT
@@ -19,6 +29,17 @@ int main(int argc, char** argv) {
   int status = RUN_ALL_TESTS();
 
   ipcl::terminateContext();
+
+#ifdef IPCL_USE_QAT
+  if (!ipcl::isQATActive())
+    std::cout << "QAT Context: INACTIVE" << std::endl;
+  else 
+    std::cout << "Error: QAT Context ACTIVE." << std::endl;
+  if (!ipcl::isQATRunning())
+    std::cout << "QAT Instances: NOT RUNNING" << std::endl;
+  else
+    std::cout << "Error: QAT Instances STILL RUNNING." << std::endl;
+#endif
 
   return status;
 }
