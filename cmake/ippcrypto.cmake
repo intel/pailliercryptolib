d48d9b9595f9c76a8e620a367a78d1d6acb7c3af
sejunkim <sejun.kim@intel.com>
2022-11-20 01:33:43 -0800

    Add feature to use pre-installed IPP-Crypto

--- cmake/ippcrypto.cmake
+++ cmake/ippcrypto.cmake
@@ -11,27 +11,30 @@ if(IPCL_IPPCRYPTO_PATH)
     set(IPPCP_SHARED OFF)
   endif()
 
+  # ippcp version 11.4 - inline with ippcp_2021.6
   find_package(ippcp 11.4 HINTS ${IPCL_IPPCRYPTO_PATH}/lib/cmake/ippcp)
 endif()
 
-
 if(ippcp_FOUND)
-  message(STATUS "------------------ IPPCRYPTO FOUND!")
+  message(STATUS "IPP-Crypto Found - using pre-installed IPP-Crypto library")
   get_target_property(IPPCRYPTO_INC_DIR IPPCP::ippcp INTERFACE_INCLUDE_DIRECTORIES)
   get_target_property(IPPCRYPTO_IMPORTED_LOCATION IPPCP::ippcp IMPORTED_LOCATION)
   get_filename_component(IPPCRYPTO_LIB_DIR ${IPPCRYPTO_IMPORTED_LOCATION} DIRECTORY)
 
-  message(STATUS "------------------ IPPCRYPTO_INC_DIR: ${IPPCRYPTO_INC_DIR}")
-  message(STATUS "------------------ IPPCRYPTO_LIB_DIR: ${IPPCRYPTO_LIB_DIR}")
-
+  install(
+    DIRECTORY ${IPPCRYPTO_LIB_DIR}/
+    DESTINATION "${IPCL_INSTALL_LIBDIR}/ippcrypto"
+    USE_SOURCE_PERMISSIONS
+  )
 
 else()
+  message(STATUS "IPP-Crypto NOT Found - building from source")
 
   set(IPPCRYPTO_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/ext_ipp-crypto)
   set(IPPCRYPTO_DESTDIR ${IPPCRYPTO_PREFIX}/ippcrypto_install)
   set(IPPCRYPTO_DEST_INCLUDE_DIR include/ippcrypto)
   set(IPPCRYPTO_GIT_REPO_URL https://github.com/intel/ipp-crypto.git)
-  set(IPPCRYPTO_GIT_LABEL ippcp_2021.6)
+  set(IPPCRYPTO_GIT_LABEL ippcp_2021.6) #ippcp version 11.4
   set(IPPCRYPTO_SRC_DIR ${IPPCRYPTO_PREFIX}/src/ext_ipp-crypto/)
 
   set(IPPCRYPTO_CXX_FLAGS "${IPCL_FORWARD_CMAKE_ARGS} -DNONPIC_LIB:BOOL=off -DMERGED_BLD:BOOL=on")
@@ -56,7 +59,6 @@ else()
               -DCMAKE_ASM_NASM_COMPILER=nasm
               -DCMAKE_BUILD_TYPE=Release
               -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-              -DCMAKE_INSTALL_INCLUDEDIR=${IPPCRYPTO_DEST_INCLUDE_DIR}
     UPDATE_COMMAND ""
     PATCH_COMMAND git apply ${CMAKE_CURRENT_LIST_DIR}/patch/ippcrypto_patch.patch
     INSTALL_COMMAND make DESTDIR=${IPPCRYPTO_DESTDIR} install
@@ -105,7 +107,6 @@ else()
       DESTINATION "${IPCL_INSTALL_LIBDIR}/ippcrypto"
       USE_SOURCE_PERMISSIONS
     )
-
 
   else()
 
